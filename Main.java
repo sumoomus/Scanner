cb657b578a0ba697fba5f05f20c7d7c16f50e16d
sumoomus <127449810+sumoomus@users.noreply.github.com>
2023-03-10 01:30:25 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+// Neo - input name
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        System.out.println("Wake up, " + input.nextLine());
+    }
+}
